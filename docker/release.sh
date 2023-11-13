faacd13a0d15783a02fd4a4354e5c52f01beed62
David Trevelyan <david.trevelyan@gmail.com>
2023-11-13 21:56:46 +0000

    Add license headers

--- docker/release.sh
+++ docker/release.sh
@@ -1,3 +1,11 @@
+#
+#    This file is part of the RealtimeSanitizer (RADSan) project.
+#    https://github.com/realtime-sanitizer/radsan
+#
+#    Copyright 2023 David Trevelyan & Alistair Barker
+#    Subject to GNU General Public License (GPL) v3.0
+#
+
 repo=realtimesanitizer/radsan-clang
 amd64_tag=${repo}:manifest-amd64
 arm64_tag=${repo}:manifest-arm64
--- examples/atomic_store_3d_coords.cpp
+++ examples/atomic_store_3d_coords.cpp
@@ -1,3 +1,11 @@
+/**
+    This file is part of the RealtimeSanitizer (RADSan) project.
+    https://github.com/realtime-sanitizer/radsan
+
+    Copyright 2023 David Trevelyan & Alistair Barker
+    Subject to GNU General Public License (GPL) v3.0
+*/
+
 #include <atomic>
 #include <iostream>
 
